30d7a0fc801ec38bfa9149e0d98272225b16b3f5
Matthias Luger <malua007@live.de>
2024-08-22 23:37:21 +0200

    initial commit

---
+++ src/client/java/com/coflnet/CoflModClient.java
@@ -0,0 +1,10 @@
+package com.coflnet;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CoflModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
