7f3359696fffd55f9e51bba8db2cfa14de7bf1ba
HackedOS <gmachina43@gmail.com>
2024-11-06 00:17:20 +0530

    Implement writing to chat and commands

--- src/client/java/com/coflnet/CoflModClient.java
+++ src/client/java/com/coflnet/CoflModClient.java
@@ -1,7 +1,12 @@
 package com.coflnet;
 
 import CoflCore.CoflCore;
-import CoflCore.events.ReceiveCommand;
+import CoflCore.classes.ChatMessage;
+import CoflCore.CoflSkyCommand;
+import CoflCore.events.OnChatMessageReceive;
+import CoflCore.events.OnModChatMessage;
+import CoflCore.events.OnWriteToChatReceive;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
@@ -11,49 +16,50 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
 import org.greenrobot.eventbus.Subscribe;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.nio.file.Path;
+
+import static com.coflnet.utils.ChatComponent;
 
 public class CoflModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
+        String username = MinecraftClient.getInstance().getSession().getUsername();
+        Path configDir = FabricLoader.getInstance().getConfigDir();
+        CoflCore cofl = new CoflCore();
+        cofl.init(configDir);
+        cofl.registerEventFile(this);
 
 		ClientPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
 			if(MinecraftClient.getInstance() != null && MinecraftClient.getInstance().getCurrentServerEntry() != null && MinecraftClient.getInstance().getCurrentServerEntry().address.contains("hypixel.net")){
 				System.out.println("Connected to Hypixel");
-				CoflCore cofl = new CoflCore();
-				try {
-					Path configDir = FabricLoader.getInstance().getConfigDir();
-
-					Path p = configDir.resolve("CoflSky");
-					if (!p.toFile().exists()) {
-						p.toFile().mkdir();
-					}
-					File configFile = new File(p.toFile(), "config.json");
-
-					cofl.setupSocket(MinecraftClient.getInstance().getSession().getUsername(), configFile.toPath());
-					cofl.registerEventFile(this);
-				} catch (IOException e) {
-					throw new RuntimeException(e);
-				} catch (URISyntaxException e) {
-					throw new RuntimeException(e);
-				}
 			}
 		});
 
-		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(ClientCommandManager.literal("cofl")
-				.executes(context -> {
-							context.getSource().sendFeedback(Text.literal("The command is executed in the client!"));
-							return 1;
-						})));
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+            dispatcher.register(ClientCommandManager.literal("cofl")
+                    .then(ClientCommandManager.argument("args", StringArgumentType.greedyString()).executes(context -> {
+                        String[] args = context.getArgument("args", String.class).split(" ");
+                        CoflSkyCommand.processCommand(args,username);
+                        return 1;
+                    })));
+        });
 
 	}
 
-	@Subscribe
-	public void onMessage(ReceiveCommand event){
-		System.out.println("Command received");
-	}
+    @Subscribe
+    public void WriteToChat(OnWriteToChatReceive command){
+         MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(ChatComponent(command.ChatMessage));
+    }
 
+    @Subscribe
+    public void onChatMessage(OnChatMessageReceive event){
+        for (ChatMessage message : event.ChatMessages) {
+            MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(ChatComponent(message));
+        }
+    }
+
+    @Subscribe
+    public void onModChatMessage(OnModChatMessage event){
+        MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.of(event.message));
+    }
 }
