d6c8a20cc41eb50c2b05ce05a06dd3eb060bb156
iroot-work <r.ian.work@outlook.com>
2025-07-11 12:32:58 +0200

    upload hovertext via chatbatch as well

--- src/client/java/com/coflnet/CoflModClient.java
+++ src/client/java/com/coflnet/CoflModClient.java
@@ -17,6 +17,8 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.nbt.*;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.text.HoverEvent;
+import net.minecraft.text.MutableText;
 import net.minecraft.util.*;
 import org.lwjgl.glfw.GLFW;
 
@@ -290,6 +292,10 @@ public class CoflModClient implements ClientModInitializer {
 
         ClientReceiveMessageEvents.ALLOW_GAME.register((message, overlay) -> {
             EventRegistry.onChatMessage(message.getString());
+            if(message.getStyle().getHoverEvent() != null
+                    && message.getStyle().getHoverEvent() instanceof HoverEvent.ShowText hest){
+                EventRegistry.onChatMessage(hest.value().getString());
+            }
             return true;
         });
 
