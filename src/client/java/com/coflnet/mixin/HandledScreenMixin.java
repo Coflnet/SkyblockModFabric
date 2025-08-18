43449285f28b71b574eca93e291eeaba3194e44d
Äkwav <ekwav@coflnet.com>
2025-08-18 12:56:24 +0200

    Make info text show in inventory

--- src/client/java/com/coflnet/mixin/HandledScreenMixin.java
+++ src/client/java/com/coflnet/mixin/HandledScreenMixin.java
@@ -6,6 +6,7 @@ import com.coflnet.gui.RenderUtils;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.client.gui.widget.MultilineTextWidget;
 import net.minecraft.client.gui.widget.ScrollableTextWidget;
 import net.minecraft.text.Text;
@@ -17,7 +18,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(HandledScreen.class)
 public abstract class HandledScreenMixin {
-    DescriptionHandler.DescModification[] openedWith = null;
     @Shadow
     protected int x;
     @Shadow
@@ -26,28 +26,28 @@ public abstract class HandledScreenMixin {
     protected int backgroundWidth;
     @Shadow
     protected int backgroundHeight;
-    private MultilineTextWidget sideTextWidget;
+    protected MultilineTextWidget sideTextWidget;
 
     @Inject(at = @At("TAIL"), method = "init")
     public void init(CallbackInfo ci){
         // init to whatever text is present
         updateText(CoflModClient.getExtraSlotDescMod());
+        String currentTitle = ((HandledScreen<?>) (Object) this).getTitle().getString();
+        if(sideTextWidget != null && !currentTitle.equals("Crafting")) {
+            sideTextWidget.setAlpha(0.3f); // make it transparent until properly loaded
+        }
         DescriptionHandler.setRefreshCallback((lines, title) -> {
 
-            System.out.println("Updating info for " + title);
+            System.out.println("Updating info for " + title + " with " + lines.length + " lines");
             updateText(CoflModClient.getExtraSlotDescMod());
         });
     }
 
-    private void updateText(DescriptionHandler.DescModification[] lines) {
-        sideTextWidget = new MultilineTextWidget(
-                15, 50,
-                Text.of("a\nx\nfor init of widget long text"),
-                MinecraftClient.getInstance().textRenderer
-        );
+    protected void updateText(DescriptionHandler.DescModification[] lines) {
+
         if(lines == null || lines.length == 0) {
             int count = lines == null ? 0 : lines.length;
-            sideTextWidget.setMessage(Text.of("no lines\nmultiline " + count));
+            sideTextWidget = null;
             return;
         }
         String temp = "";
@@ -68,24 +68,18 @@ public abstract class HandledScreenMixin {
                 continue;
             lineCount++;
         }
-        sideTextWidget.setMessage(Text.of(temp));
-        sideTextWidget.setPosition(x - maxWidth - 5, y+5);
+        sideTextWidget = new MultilineTextWidget(
+                x - maxWidth - 5, y+5,
+                Text.of(temp),
+                MinecraftClient.getInstance().textRenderer
+        );
         sideTextWidget.setDimensions(maxWidth + 10, lineCount * 20);
         sideTextWidget.setAlpha(0.9f);
     }
 
-    @Inject(at = @At("RETURN"), method = "render")
-    public void render(DrawContext context, int mouseX, int mouseY, float deltaTicks, CallbackInfo ci){
-        DescriptionHandler.DescModification[] currentText = CoflModClient.getExtraSlotDescMod();
-        if(currentText.length == 0)
-            return;
+    @Inject(at = @At("HEAD"), method = "render")
+    public void renderMain(DrawContext context, int mouseX, int mouseY, float deltaTicks, CallbackInfo ci){
         if(sideTextWidget != null)
             sideTextWidget.render(context, mouseX, mouseY, deltaTicks);
-        if(openedWith == null || openedWith != currentText) {
-            updateText(currentText);
-            System.out.println("Updating side text widget with new data");
-            openedWith = currentText;
-        }
     }
-
 }
