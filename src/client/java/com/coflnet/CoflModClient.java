40f7f3770211af8fdff798172a6c3a44b9039dc8
Äkwav <ekwav@coflnet.com>
2026-03-29 14:30:55 +0200

    remove fabric duplicate apis

--- src/client/java/com/coflnet/CoflModClient.java
+++ src/client/java/com/coflnet/CoflModClient.java
@@ -1516,9 +1516,14 @@ public class CoflModClient implements ClientModInitializer {
     public static void cacheMods() {
         ModListData modListData = new ModListData();
         for (net.fabricmc.loader.api.ModContainer mod : FabricLoader.getInstance().getAllMods()) {
-            modListData.addModname(mod.getMetadata().getName());
-            modListData.addFilename(mod.getMetadata().getName());
-            modListData.addFilename(mod.getMetadata().getId());
+            String modName = mod.getMetadata().getName();
+            String modId = mod.getMetadata().getId();
+            if (modName.startsWith("Fabric ") || modId.startsWith("fabric")) {
+                continue;
+            }
+            modListData.addModname(modName);
+            modListData.addFilename(modName);
+            modListData.addFilename(modId);
         }
         CoflCore.Wrapper.SendMessage(new RawCommand("foundMods", new Gson().toJson(modListData)));
     }
